967bd9cc68b3cbee9f9b76d2c06d5f316a04239d
Glubus <osefcode@gmail.com>
2025-09-18 00:09:46 +0200

    refactor: make clippy happy :)

--- src/models/failed_query/impl.rs
+++ src/models/failed_query/impl.rs
@@ -4,22 +4,19 @@ use sqlx::{Error as SqlxError, PgPool};
 
 impl FailedQueryRow {
     pub async fn insert(pool: &PgPool, hash: &str) -> Result<i32, SqlxError> {
-        let result = insert(pool, hash).await?;
-        Ok(result)
+        insert(pool, hash).await
     }
 
     pub async fn exists_by_hash(pool: &PgPool, hash: &str) -> Result<bool, SqlxError> {
-        let result = exists_by_hash(pool, hash).await?;
-        Ok(result)
+        exists_by_hash(pool, hash).await
     }
 
     pub async fn find_by_id(pool: &PgPool, id: i32) -> Result<Self, SqlxError> {
-        let result = find_by_id(pool, id).await?;
-        Ok(result.ok_or(SqlxError::RowNotFound)?)
+        find_by_id(pool, id).await?.ok_or(SqlxError::RowNotFound)
+
     }
 
     pub async fn delete_by_hash(pool: &PgPool, hash: &str) -> Result<u64, SqlxError> {
-        let result = delete_by_hash(pool, hash).await?;
-        Ok(result)
+        delete_by_hash(pool, hash).await
     }
 }
