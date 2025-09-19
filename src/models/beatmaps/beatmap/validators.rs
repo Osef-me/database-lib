12c3daf63f2099da632b029833e5abe1f49d5f64
Osef <Glubus>
2025-09-19 10:55:53 +0200

    fix

--- src/models/beatmaps/beatmap/validators.rs
+++ src/models/beatmaps/beatmap/validators.rs
@@ -1,8 +1,6 @@
 use bigdecimal::{BigDecimal, FromPrimitive};
 use validator::ValidationError;
 
-
-
 pub fn validate_od_hp_cs(value: &BigDecimal) -> Result<(), ValidationError> {
     if value < &BigDecimal::from_f64(0.0).unwrap() || value > &BigDecimal::from_f64(10.0).unwrap() {
         return Err(validator::ValidationError::new("invalid_od_hp_cs"));
